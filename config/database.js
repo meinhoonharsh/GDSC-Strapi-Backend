abe435d2577fc85e0ff9e8d6331b6052a23cb9b5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-11 23:24:27 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,26 @@
-const path = require('path');
+// postgres://dugzqbfosyrpxb:83b8e1eff8c6716ed1bf5f5cdd6c6859e664a94cf676a7693b29fc0f3c836d28@ec2-3-225-41-234.compute-1.amazonaws.com:5432/de8uet3q2prros
+// postgres://{user}:{password}@{hostname}:{port}/{database-name}
 
+const config = {
+    user: 'dugzqbfosyrpxb',
+    database: 'de8uet3q2prros',
+    password: '83b8e1eff8c6716ed1bf5f5cdd6c6859e664a94cf676a7693b29fc0f3c836d28',
+    port: 5432,
+    host: 'ec2-3-225-41-234.compute-1.amazonaws.com',
+}   
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
