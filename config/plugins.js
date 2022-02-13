bbf0c7eabb215c1cf52f1ba739e6b1ddb92410e9
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-14 01:36:04 +0530

    Implemented Cloundinary Storage

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "dnvhrphtn",
+        api_key: "174573523397788",
+        api_secret: "_9qsqh51j7qS5g5qqc_y5beofsA",
+      },
+    },
+  },
+});
